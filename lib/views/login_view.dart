0daddb12615f993fee78aaf7949ba5feb03a2604
Atif <atif.hussain.khan@hotmail.com>
2023-08-31 17:05:36 +0100

    create new route for notes and implement navigation from login screen to notes screen once logged in

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -55,10 +55,14 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential = await FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                        email: email, password: password);
-                devtools.log(userCredential.toString());
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  '/notes/',
+                  (route) => false,
+                );
               } on FirebaseAuthException catch (e) {
                 switch (e.code) {
                   case 'missing-email':
