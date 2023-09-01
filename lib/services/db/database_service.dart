916e4b8052c59d4dc694d788e2f816ad6fa084cb
Atif <atif.hussain.khan@hotmail.com>
2023-09-01 17:12:40 +0100

    create getOrCreateUser function in DatabaseService

--- lib/services/db/database_service.dart
+++ lib/services/db/database_service.dart
@@ -1,5 +1,5 @@
+import 'dart:async';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/db/database_exceptions.dart';
 import 'package:sqflite/sqflite.dart';
@@ -53,6 +53,25 @@ class DatabaseNote {
 
 class DatabaseService {
   Database? _db;
+  List<DatabaseNote> _notes = [];
+
+  final _notesStreamController = StreamController<List<DatabaseNote>>.broadcast();
+
+  Future<DatabaseUser> getOrCreateUser({required String email }) async {
+    try {
+      return await getUser(email: email);
+    } on UserDoesNotExistException{
+      return await createUser(email: email);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> _cacheNotes() async {
+    final allNotes = await getAllNotes();
+    _notes = allNotes;
+    _notesStreamController.add(allNotes);
+  }
 
   Future<DatabaseNote> createNote({required DatabaseUser owner}) async {
     final db = _getDatabase();
@@ -62,28 +81,39 @@ class DatabaseService {
     const text = '';
     final noteRow = {userIdCol: owner.id, textCol: text};
     final noteId = await db.insert(noteTable, noteRow);
-
-    return DatabaseNote(id: noteId, userId: owner.id, text: text);
+    final newNote = DatabaseNote(id: noteId, userId: owner.id, text: text);
+    _notes.add(newNote);
+    _notesStreamController.add(_notes);
+    return newNote;
   }
 
   Future<int> deleteNote({required int id, bool deleteAll = false}) async {
     final db = _getDatabase();
-    final int deletedNote;
+    final int numberOfDeletions;
     if (deleteAll) {
-      deletedNote = await db.delete(noteTable);
-      if (deletedNote > 0) throw CannotDeleteNoteException();
+      numberOfDeletions = await db.delete(noteTable);
+      if (numberOfDeletions > 0) throw CannotDeleteNoteException();
+      _notes = [];
+      _notesStreamController.add(_notes);
     } else {
-      deletedNote = await db.delete(noteTable, where: '$idCol = ?', whereArgs: [id]);
-      if (deletedNote != 1) throw CannotDeleteNoteException();
+      numberOfDeletions = await db.delete(noteTable, where: '$idCol = ?', whereArgs: [id]);
+      if (numberOfDeletions != 1) throw CannotDeleteNoteException();
+      _notes.removeWhere((note) => note.id == id);
+      _notesStreamController.add(_notes);
     }
-    return deletedNote;
+    return numberOfDeletions;
   }
 
   Future<DatabaseNote> getNote({required int id}) async {
     final db = _getDatabase();
-    final note = await db.query(noteTable, limit: 1, where: '$idCol = ?', whereArgs: [id]);
-    if (note.isEmpty) throw NoteDoesNotExistException();
-    return DatabaseNote.fromRow(note.first);
+    final noteMap = await db.query(noteTable, limit: 1, where: '$idCol = ?', whereArgs: [id]);
+    if (noteMap.isEmpty) throw NoteDoesNotExistException();
+    final note = DatabaseNote.fromRow(noteMap.first);
+    // update local cache (ie. the note in cache might be out of date)
+    _notes.removeWhere((note) => note.id == id);
+    _notes.add(note);
+    _notesStreamController.add(_notes);
+    return note;
   }
 
   Future<List<DatabaseNote>> getAllNotes() async {
@@ -96,12 +126,16 @@ class DatabaseService {
   Future<DatabaseNote> updateNote({required DatabaseNote note, required String text}) async {
     final db = _getDatabase;
     await getNote(id: note.id);
-    final updatedNote = {
+    final update = {
       textCol: text
     };
-    final updateCount = await db.call().update(noteTable, updatedNote);
+    final updateCount = await db.call().update(noteTable, update);
     if (updateCount == 0) throw CannotUpdateNoteDatabaseException();
-    return await getNote(id: note.id);  
+    final updatedNote = await getNote(id: note.id);
+    _notes.removeWhere((note) => note.id == updatedNote.id);
+    _notes.add(updatedNote);
+    _notesStreamController.add(_notes);
+    return updatedNote;  
   }
 
   Database _getDatabase() {
@@ -142,6 +176,7 @@ class DatabaseService {
       _db = db;
       await db.execute(createUserTableQuery);
       await db.execute(createNoteTableQuery);
+      await _cacheNotes();
     } on MissingPlatformDirectoryException {
       throw UnableToGetDirectoryDatabaseException();
     } catch (e) {
